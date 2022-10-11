abd233146ff3ca3117808235457f1a9393040c2e
Andy Curtis <acurtis166@gmail.com>
2022-10-10 23:44:58 -0500

    packaging updates

--- halo_infinite_api/__init__.py
+++ halo_infinite_api/__init__.py
@@ -1,3 +1,3 @@
-
+"""Python API for requesting Halo Infinite multiplayer data"""
 
 __version__ = '0.1.0'
---
+++ scripts/editable_install.bat
@@ -0,0 +1,2 @@
+:: Install package locally in editable mode (call from root dir)
+python -m pip install -e .
